b81a1c1e206c9d1a7b652b1e78fb8f5af949777c
samir bertel <samir@kali.lenovo>
2026-01-22 20:46:40 -0500

    Cambio en api.ts

--- autoclub-frontend/app/config/api.ts
+++ autoclub-frontend/app/config/api.ts
@@ -1,2 +1,3 @@
-// app/config/api.ts
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+// Usar SIEMPRE la variable de entorno
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const response = await axios.post(`${API_URL}/auth/login`, data);
